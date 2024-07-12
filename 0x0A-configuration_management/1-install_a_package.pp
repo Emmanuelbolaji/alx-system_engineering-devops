8d18001e31940afdf4077d3df0566ef7104c59f5
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-07-12 09:06:29 +0000

    puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Installs Flask version 2.1.0 and Werkzeug version 2.0.1 using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
+}

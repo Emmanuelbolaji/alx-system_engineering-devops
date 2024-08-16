4815fb98be29410f97f840714eabf5a1d04e5746
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-08-16 07:31:10 +0000

    puppett

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
 # Fixes a WordPress file with wrong filename
-file { '/var/www/html':
-ensure => 'directory',
-}
-
-file { '/var/www/html/wp-settings.php':
-  ensure  => 'present',
-  content => '<?php /* Placeholder content */ ?>',
-  require => File['/var/www/html'],
-}
-
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
-  require => File['/var/www/html/wp-settings.php'],
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
 }
